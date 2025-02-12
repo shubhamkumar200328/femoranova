82ba01b6e2138ef2e6942d5e2d37842dd26c044e
shubhamkumar200328 <shubhamkumar200328@gmail.com>
2025-02-12 09:10:51 +0530

    Added logo and movified some css in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link"
+import logo from "@/assets/femoranova_logo.png"
+import Image from "next/image"
 
 export default function Navbar() {
   return (
     <nav className="w-full bg-white shadow-lg fixed top-0 z-50">
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold text-purple-600">
-          Femora
-        </Link>
+      <div className="container mx-auto px-6 py-1 flex justify-between items-center">
+        <div className="logoClass space-x-6 flex flex-row items-center">
+          <Image src={logo.src} alt="logo" width={54} height={54} priority />
+          <Link href="/" className="text-2xl font-bold text-purple-600">
+            Femora
+            <label className="text-2xl font-bold text-pink-600">nova</label>
+          </Link>
+        </div>
         <div className="space-x-6">
           <Link href="/about" className="text-gray-700 hover:text-purple-600">
             About
@@ -19,6 +25,9 @@ export default function Navbar() {
           </Link>
           <Link href="/contact" className="text-gray-700 hover:text-purple-600">
             Contact
+          </Link>
+          <Link href="/login" className="text-gray-700 hover:text-purple-600">
+            login
           </Link>
         </div>
       </div>
