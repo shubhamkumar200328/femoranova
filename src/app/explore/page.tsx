0cf5128191b779f1dd9c4e32b463fc016610b8e2
shubhamkumar200328 <shubhamkumar200328@gmail.com>
2025-02-21 05:01:17 +0530

    added explore and sos

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,13 +1,11 @@
 import Navbar from "@/components/Navbar"
 import Sos from "./sos/page"
-import Footer from "@/components/Footer"
 
 export default function explore() {
   return (
     <>
       <Navbar />
       <Sos />
-      {/* <Footer /> */}
     </>
   )
 }
